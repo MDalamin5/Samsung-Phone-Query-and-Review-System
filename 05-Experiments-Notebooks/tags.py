8cbf0c4f2933461738705a429655544e77faf5d4
MDalamin5 <mdal.amin5@northsouth.edu>
2025-06-19 23:45:37 +0600

    play with bs4

--- 05-Experiments-Notebooks/tags.py
+++ 05-Experiments-Notebooks/tags.py
@@ -8,4 +8,41 @@ with open("sample.html", "r") as f:
     
 soup = BeautifulSoup(html_docs, "html.parser")
 
-print(soup.prettify())
+# print(soup.prettify())
+# print(soup.div) # for single div
+
+# print(soup.find_all("div"))
+# print(soup.find_all("div")[2])
+
+# print(soup.find(class_=="hello1"))
+
+# for link in soup.find_all("a"):
+#     print(link)
+
+# for link in soup.find_all("a"):
+#     # print(link.string)
+#     print(link.get_text())
+
+# s = soup.find(id="link3")
+# print(s.string)
+# print(s.get_text())
+
+# print(soup.select_one("div.hello"))
+
+# print(soup.div.get("class"))
+
+# print(soup.span.get("class"))
+
+# print(soup.find(class_="hello1"))
+
+# for child in soup.find(id="container").children:
+#     print(child)
+
+i = 1
+
+for parent in soup.find(class_="box").parents:
+    print(parent.get_text())
+    if i == 2:
+        break
+    i += 1
+    
