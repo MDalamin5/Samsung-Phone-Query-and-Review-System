7abf168d3ed2895b26e97bbfffe78a327c7de6cb
MDalamin5 <mdal.amin5@northsouth.edu>
2025-06-20 20:31:59 +0600

    extract data by web scraping and preprocess and save into the db

---
+++ 05-Experiments-Notebooks/text-data/file-list.py
@@ -0,0 +1,23 @@
+all_html_file = [
+    "samsung_galaxy_a06_5g.html",
+    "samsung_galaxy_a16_5g.html",
+    "samsung_galaxy_a16.html",
+    "samsung_galaxy_a26.html",
+    "samsung_galaxy_a35.html",
+    "samsung_galaxy_a36.html",
+    "samsung_galaxy_a55.html",
+    "samsung_galaxy_a56.html",
+    "samsung_galaxy_c55.html",
+    "samsung_galaxy_f06_5g.html",
+    "samsung_galaxy_f15.html",
+    "samsung_galaxy_f16.html",
+    "samsung_galaxy_m06.html",
+    "samsung_galaxy_m15.html",
+    "samsung_galaxy_m16.html",
+    "samsung_galaxy_m55.html",
+    "samsung_galaxy_s23_ultra.html",
+    "samsung_galaxy_s23.html",
+    "samsung_galaxy_s24.html",
+    "samsung_galaxy_s25.html",
+    "samsung_galaxy_s25_ultra.html"
+]
