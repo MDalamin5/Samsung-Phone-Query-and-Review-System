6823806ac746217d786ddcc3414cd2194e5e4371
MDalamin5 <mdal.amin5@northsouth.edu>
2025-06-24 15:56:57 +0600

    samsung phone qna and review system implementation using RAG, and multiagent

--- samsung_assistant/samsung_assistant/config.py
+++ samsung_assistant/samsung_assistant/config.py
@@ -5,6 +5,11 @@ from dotenv import load_dotenv
 def load_environment():
     """Loads environment variables from .env file."""
     load_dotenv()
+    
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    os.environ["LANGCHAIN_PROJECT"] = "Samsung Phone Assistant"
+    
+    print("LangSmith tracing is enabled for project: 'Samsung Phone Assistant'")
 
 # Call it once at the start
 load_environment()
@@ -13,3 +18,4 @@ load_environment()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 HF_TOKEN = os.getenv("HF_TOKEN")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
